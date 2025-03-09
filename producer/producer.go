ffac25c4898db6ed70e4a308bc8b71b5b6df6255
Vishal Anarase <iamvishalanarase@gmail.com>
2025-03-10 00:05:04 +0530

    Update code for producer to use core go client
    Signed-off-by: Vishal Anarase <iamvishalanarase@gmail.com>

--- producer/producer.go
+++ producer/producer.go
@@ -1,38 +1,63 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
-	"time"
+	"math/rand"
+	"os"
 
-	"github.com/segmentio/kafka-go"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
-	// Create Kafka writer
-	writer := kafka.NewWriter(kafka.WriterConfig{
-		Brokers:  []string{"localhost:9092"},
-		Topic:    "user-registered",
-		Balancer: &kafka.LeastBytes{},
+	// Create a new producer instance
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"acks":              "all",
 	})
 
-	defer writer.Close()
-
-	// Produce messages to Kafka topic
-	for i := 0; i < 5; i++ {
-		msg := fmt.Sprintf("User registered %d: John Doe", i)
-		kmsg := kafka.Message{
-			Value: []byte(msg),
-		}
-
-		err := writer.WriteMessages(context.Background(), kmsg)
-		if err != nil {
-			log.Printf("Failed to send message: %v", err)
-		} else {
-			log.Println(msg)
-		}
-
-		time.Sleep(1 * time.Second) // Adding delay between messages
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s", err)
+		os.Exit(1)
 	}
+
+	// Run a goroutine to handle events from the producer's event channel
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Produced event to topic %s: key = %-10s value = %s\n",
+						*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
+				}
+			}
+		}
+	}()
+
+	// Define some data
+	users := [...]string{"eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther"}
+	items := [...]string{"book", "alarm clock", "t-shirts", "gift card", "batteries"}
+
+	// Topic name
+	topic := "purchases"
+
+	for n := 0; n < 10; n++ {
+		key := users[rand.Intn(len(users))]
+		data := items[rand.Intn(len(items))]
+
+		// Enqueue message into the queue
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic:     &topic,
+				Partition: kafka.PartitionAny,
+			},
+			Key:   []byte(key),
+			Value: []byte(data),
+		}, nil)
+	}
+
+	// Wait for all messages to be delivered
+	p.Flush(15 * 1000)
+	p.Close()
 }
